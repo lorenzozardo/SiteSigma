9d814cfc7e6761192cf7dbef77173f46eaa6a78e
Lorenzo Zardo <92864867+lorenzozardo@users.noreply.github.com>
2022-06-02 12:30:19 -0300

    Add files via upload

---
+++ sitesigma/includes/_dados.php
@@ -0,0 +1,14 @@
+<?php
+
+$produtos = array(
+    0 => array('nome'=>'Elefante Legal', 'preco'=>'1200.00','descricao'=>'Descrição Elefante Legal'imagem'=>'elefantelegal.jpg'),
+    1 => array('nome'=>'Cachrro Legal', 'preco'=>'','descricao'=>''imagem'=>''),
+    2 => array('nome'=>'', 'preco'=>'','descricao'=>''imagem'=>''),
+    3 => array('nome'=>'', 'preco'=>'','descricao'=>''imagem'=>''),
+    4 => array('nome'=>'', 'preco'=>'','descricao'=>''imagem'=>''),
+    5 => array('nome'=>'', 'preco'=>'','descricao'=>''imagem'=>''),
+    6 => array('nome'=>'', 'preco'=>'','descricao'=>''imagem'=>''),
+    7 => array('nome'=>'', 'preco'=>'','descricao'=>''imagem'=>''),
+    8 => array('nome'=>'', 'preco'=>'','descricao'=>''imagem'=>''),
+    9 => array('nome'=>'', 'preco'=>'','descricao'=>''imagem'=>''),
+);
