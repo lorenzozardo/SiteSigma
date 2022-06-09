6d74f29b45ff1a0d8e4c4bf0132df997978f3679
Lorenzo Zardo <92864867+lorenzozardo@users.noreply.github.com>
2022-06-09 12:18:40 -0300

    Add files via upload

--- sitesigma/includes/_dados.php
+++ sitesigma/includes/_dados.php
@@ -1,14 +1,15 @@
 <?php
 
 $produtos = array(
-    0 => array('nome'=>'Elefante Legal', 'preco'=>'1200.00','descricao'=>'Descrição Elefante Legal'imagem'=>'elefantelegal.jpg'),
-    1 => array('nome'=>'Cachrro Legal', 'preco'=>'','descricao'=>''imagem'=>''),
-    2 => array('nome'=>'', 'preco'=>'','descricao'=>''imagem'=>''),
-    3 => array('nome'=>'', 'preco'=>'','descricao'=>''imagem'=>''),
-    4 => array('nome'=>'', 'preco'=>'','descricao'=>''imagem'=>''),
-    5 => array('nome'=>'', 'preco'=>'','descricao'=>''imagem'=>''),
-    6 => array('nome'=>'', 'preco'=>'','descricao'=>''imagem'=>''),
-    7 => array('nome'=>'', 'preco'=>'','descricao'=>''imagem'=>''),
-    8 => array('nome'=>'', 'preco'=>'','descricao'=>''imagem'=>''),
-    9 => array('nome'=>'', 'preco'=>'','descricao'=>''imagem'=>''),
+    0 => array('nome'=>'Elefante Legal', 'preco'=>'1200.00','descricao'=>'Descrição Elefante Legal', 'imagem'=>'elefantelegal.jpg'),
+    1 => array('nome'=>'Cachrro Legal', 'preco'=>'','descricao'=>'', 'imagem'=>''),
+    2 => array('nome'=>'Produto A', 'preco'=>'','descricao'=>'', 'imagem'=>''),
+    3 => array('nome'=>'Produto B', 'preco'=>'','descricao'=>'', 'imagem'=>''),
+    4 => array('nome'=>'Produto C', 'preco'=>'','descricao'=>'', 'imagem'=>''),
+    5 => array('nome'=>'Produto D', 'preco'=>'','descricao'=>'', 'imagem'=>''),
+    6 => array('nome'=>'Produto E', 'preco'=>'','descricao'=>'', 'imagem'=>''),
+    7 => array('nome'=>'Produto F', 'preco'=>'','descricao'=>'', 'imagem'=>''),
+    8 => array('nome'=>'Produto G', 'preco'=>'','descricao'=>'', 'imagem'=>''),
+    9 => array('nome'=>'Produto H', 'preco'=>'','descricao'=>'', 'imagem'=>'')
 );
+?>
--- sitesigma/index.php
+++ sitesigma/index.php
@@ -6,6 +6,19 @@ include_once './includes/_header.php';
 ?>
 
 <h1>Home</h1>
+<?php
+    for ($i=0; $i < 3; $i++) { 
+        echo $i. '<br>';
+    echo $produtos[$i]['nome'];
+    echo $produtos[$i]['preco'];
+    echo '<img src="./content/'.$produtos[$i]['imagem'].'">';
+    echo '<hr>';
+    }
+
+?>
+
+
+
 
 <?php
 // include do footer
