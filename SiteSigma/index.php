d69e1d6903c7cf77362aa0be85cf2744a58dfcf1
Lorenzo Zardo <92864867+lorenzozardo@users.noreply.github.com>
2022-06-23 12:20:19 -0300

    Add files via upload

---
+++ SiteSigma/index.php
@@ -0,0 +1,37 @@
+<?php
+// include do footer
+include_once './includes/_dados.php';
+include_once './includes/_head.php';
+include_once './includes/_header.php';
+?>
+<link rel="stylesheet" href="style.css">
+<div class="container">
+    <br>
+    <h2>Promoção</h2>
+    <div class="row mt-4">
+        <?php
+
+        // laco de repeticao para exibir os 3 primeiros produtos
+    for ($i=0; $i < 3 ; $i++) { 
+    ?>
+        <div class="card m-4" style="width: 18rem;">
+    <img src="./content/<?php echo $produtos[$i]['imagem'];?>" class="card-img-top" alt="...">
+    <div class="card-body">
+        <h5 class="card-title"><?php echo $produtos[$i]['nome'];?></h5>
+        <p class="card-text"><?php echo $produtos[$i]['descricao'];?></p>
+        <a href="produto-detalhe.php?id=<?php echo $i?>&tipo=promocao" class="btn btn-primary">Comprar</a>
+    </div>
+</div>
+
+        <?php
+    }
+    ?>
+
+    </div>
+</div>
+
+
+<?php
+// include do footer
+include_once './includes/_footer.php';
+?>
