6bec4ea9685df54f8f29c3040d28cd24f7de9c9c
Lorenzo Zardo <92864867+lorenzozardo@users.noreply.github.com>
2022-06-30 12:24:50 -0300

    Add files via upload

--- SiteSigma/contato.php
+++ SiteSigma/contato.php
@@ -3,9 +3,46 @@
 include_once './includes/_dados.php';
 include_once './includes/_head.php';
 include_once './includes/_header.php';
+
+/**
+ * $_REQUEST é a mesma coisa que $_GET e $_POST juntos
+ *
+ */
+
+if (isset($_POST['txtNome'])) {
+    $nome = strtoupper($_POST['txtNome']);
+    echo $nome;
+}
+if (isset($_POST['txtEmail'])) {
+    $nome = strtoupper($_POST['txtEmail']);
+    echo $nome;
+}
+if (isset($_POST['txtTelefone'])) {
+    $nome = strtoupper($_POST['txtTelefone']);
+    echo $nome;
+}
+if (isset($_POST['txtMensagem'])) {
+    $nome = strtoupper($_POST['txtMensagem']);
+    echo $nome;
+}
 ?>
 
 <h1>Contato</h1>
+<form action="./contato.php" method="post">
+    <label for="txtNome">Nome Completo</label>
+    <input type="text" name="txtNome" id="txtNome">
+<br>
+    <label for="txtEmail">E-mail</label>
+    <input type="email" name="txtEmail" id="txtEmail">
+<br>
+    <label for="txtEmail">Telefone</label>
+    <input type="tel" name="txtTelefone" id="txtTelefone">
+<br>
+    <label for="txtEmail">Mensagem</label>
+    <input type="text" name="txtMensagem" id="txtMensagem">
+<br>
+<input type="submit" value="Cadastrar">
+</form>
 
 <?php
 // include do footer
