0cdf6007ce74add1fda462549d52a4c2d681bc2c
Lorenzo Zardo <92864867+lorenzozardo@users.noreply.github.com>
2022-07-05 12:19:51 -0300

    Add files via upload

---
+++ Site_Sigma/includes/_dados.php
@@ -0,0 +1,5 @@
+<?php
+$produtos = array
+0=> array('nome'=>'labrador', 'preco'=> '1200.00','descrição'=>'Descrição labrador','imagem')
+1=> array('nome'=>'labrador', 'preco'=> '1200.00','descrição'=>'Descrição labrador','imagem')
+2=> array('nome'=>'labrador', 'preco'=> '1200.00','descrição'=>'Descrição labrador','imagem')
