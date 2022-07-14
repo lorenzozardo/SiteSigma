783c0a365c54c4b57cba6a30e81536b8c4dde86d
Lorenzo Zardo <92864867+lorenzozardo@users.noreply.github.com>
2022-07-14 12:33:33 -0300

    Add files via upload

--- SiteSigma/db/sitesigma.sql
+++ SiteSigma/db/sitesigma.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 07-Jul-2022 às 16:40
+-- Tempo de geração: 14-Jul-2022 às 17:31
 -- Versão do servidor: 10.4.17-MariaDB
 -- versão do PHP: 7.3.27
 
@@ -46,7 +46,8 @@ INSERT INTO `categorias` (`CategoriaID`, `Nome`, `Ativo`) VALUES
 (7, 'Capivara', 1),
 (8, 'Rato', 1),
 (9, 'Leão', 1),
-(10, 'Macaco', 1);
+(10, 'Macaco', 1),
+(11, 'Computador', 1);
 
 -- --------------------------------------------------------
 
@@ -63,6 +64,16 @@ CREATE TABLE `produtos` (
   `CategoriaID` int(11) NOT NULL,
   `Ativo` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Extraindo dados da tabela `produtos`
+--
+
+INSERT INTO `produtos` (`Produto`, `Nome`, `Descricao`, `Imagem`, `Preco`, `CategoriaID`, `Ativo`) VALUES
+(1, '#5634', 'By Bored Ape Yacht Club', '5634.jpg', 1520040000, 1, 1),
+(2, '#9607', 'By Bored Ape Yacht Club', '9607.jpg', 1246000000, 2, 1),
+(3, '#7638', 'By Bored Ape Yacht Club', '7638.jpg', 1320040000, 3, 1),
+(4, '#648', 'By Bored Ape Yacht Club', '648.jpg', 1023000000, 4, 1);
 
 --
 -- Índices para tabelas despejadas
@@ -88,13 +99,13 @@ ALTER TABLE `produtos`
 -- AUTO_INCREMENT de tabela `categorias`
 --
 ALTER TABLE `categorias`
-  MODIFY `CategoriaID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `CategoriaID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- AUTO_INCREMENT de tabela `produtos`
 --
 ALTER TABLE `produtos`
-  MODIFY `Produto` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `Produto` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
