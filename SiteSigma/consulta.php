d1300921ff403c431e711a03de03e14de83eedf6
Lorenzo Zardo <92864867+lorenzozardo@users.noreply.github.com>
2022-07-18 21:34:32 -0300

    Delete SiteSigma directory

--- SiteSigma/consulta.php
+++
@@ -1,8 +0,0 @@
-<?php
-// include do footer
-include_once './includes/_banco.php';
-include_once './includes/_footer.php';
-include_once './includes/_head.php';
-include_once './includes/_header.php';
-?>
-<h1>Consulta</h1>
